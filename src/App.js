e29fa8651657037995045c40fdc71a76e839f1e5
Tristan Bennett <betriistan@gmail.com>
2019-05-04 17:49:54 -0500

    added state and conditonally displayed content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-	render() {
-		return (
-			<div>
+    constructor() {
+        super();
+        this.state = {
+            displayBio: true
+        };
+    }
+
+    render() {
+
+        return (
+            <div>
 				<h1>Hiya!</h1>
 				<p>My name is Tristan.</p>
-				<p>I'm a multicultural full stack software engineer, polyglot, and ideator, always ready to learn something new!</p> 
+				<p>I'm a multicultural full stack software engineer, polyglot, and ideator, always ready to learn something new!</p>
+				{
+					this.state.displayBio ? (
+						<div>
+							<p>I live in Austin, and ride my bike everywhere I go.</p>
+							<p>My forte is the MERN stack, but I also love Vue</p>
+						</div>
+					) : null
+				}
 			</div>
-		)
-	}
+        )
+    }
 }
