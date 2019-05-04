6e4cc4dd09d565ca7b13c1dc6cec998246b4b694
Tristan Bennett <betriistan@gmail.com>
2019-05-04 17:23:38 -0500

    added app.js and basic content

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hiya!</h1>
+				<p>My name is Tristan.</p>
+				<p>I'm a multicultural full stack software engineer, polyglot, and ideator, always ready to learn something new!</p> 
+			</div>
+		)
+	}
+}
