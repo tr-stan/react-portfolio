0f09938584a247bd8e1586b6e3c39472f405b370
Tristan Bennett <betriistan@gmail.com>
2019-05-08 10:17:31 -0500

    updated click event for read more button, added assets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            displayBio: false
-        };
-        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
-    }
+	state = { displayBio: false };
 
-    toggleDisplayBio() {
+    toggleDisplayBio = () => {
     	this.setState({ displayBio: !this.state.displayBio });
     }
-
 
     render() {
 
---
+++ src/data/projects.js
@@ -0,0 +1,13 @@
+import spectrofy from '../assets/spectrofy.gif'
+import mazerLazer from '../assets/mazer-lazer.png'
+import localForecast from '../assets/local-forecast.png'
+import allGreek from '../assets/all-greek.png'
+
+// use 'screencase' syntax for global variables
+const PROJECTS = [
+	{
+		title: "Random Generator: It's All Greek to Me",
+		description: "An app I made for the freeCodeCamp curriculum that randomly generates different translations of the idiom 'It\'s all Greek to me' and their transliterations. For this app many people used third-party APIs to source their random quotes, but I thought it would be more interesting to create my own object of translated idioms instead and randomly generate the idiom in different langauges.",
+		link: ""
+	}
+];
