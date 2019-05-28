806e695ed80100af53aaaf14cec4bb0ccf8df022
Tristan Bennett <betriistan@gmail.com>
2019-05-28 17:56:04 -0500

    installed react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import App from './Components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
