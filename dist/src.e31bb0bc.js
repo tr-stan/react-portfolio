7d7829ec595d08b073c47274acac79297cd58946
Tristan Bennett <betriistan@gmail.com>
2019-06-29 23:30:19 -0500

    improved responsiveness of revolving title

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -31988,7 +31988,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54192" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54899" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
