34b14ef22c4368d629d3e93ea9b8003576f61c51
Tristan Bennett <betriistan@gmail.com>
2020-07-22 11:39:45 -0500

    update project source links

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,7 +9,7 @@ const PROJECTS = [
 		id: 1,
 		title: "Spectrofy",
 		description: "An application that allows users to search for songs and see generative bubble visuals based on the song's audio analysis data from Spotify's Web API.",
-		link: "https://spectrofy.netlify.com/",
+		link: "https://spectrofy.netlify.app/",
 		image: spectrofy
 	},
 	{
@@ -23,14 +23,14 @@ const PROJECTS = [
 		id: 3,
 		title: "Local Weather App",
 		description: "A simple local weather app that displays local weather conditions in Fahrenheit or Celsius using JSON data from the openweathermap API.",
-		link: "https://s.codepen.io/tri-be/debug/eZLqzW/mWMoNxVpEQLk",
+		link: "https://cdpn.io/tri-be/debug/eZLqzW/yoMZEOmLDoyk",
 		image: localForecast
 	},
 	{
 		id: 4,
 		title: "Random Generator - It's All Greek to Me",
 		description: "An app I made for the freeCodeCamp curriculum that randomly generates different translations of the idiom 'It's all Greek to me' and their transliterations. For this app many people used third-party APIs to source their random quotes, but I thought it would be more interesting to create my own object of translated idioms instead and randomly generate the idiom in different langauges.",
-		link: "https://s.codepen.io/tri-be/debug/groJby/LQkExKGOONEA",
+		link: "https://cdpn.io/tri-be/debug/groJby/XBkGRqwnWXVA",
 		image: allGreek
 	}
 ];
